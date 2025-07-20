2efb495ce1c8706eaa04b620fa7d73170a2f3c3c
Mustafa Serhat Aydın <aydinmustafaserhat@gmail.com>
2025-07-21 00:23:43 +0300

    Update plotting.py
    added morlet transform

--- functions/plotting.py
+++ functions/plotting.py
@@ -2,6 +2,7 @@ import numpy as np
 from mne.time_frequency import psd_array_welch
 from scipy.signal import spectrogram
 import matplotlib.pyplot as plt
+import pywt
 
 # ecog_data is the output of extract_neural_data, freq_range is a tuple (a, b), where a < b
 def psd_welch(ecog_data, lower_freq, upper_freq, n_per_seg=128):
@@ -54,16 +55,12 @@ def plot_spectrogram(ecog_data, lower_freq, upper_freq, nperseg=256, baseline_co
         mu = baseline_power
         sig = Sxx[:, baseline_mask].std(axis=1, keepdims=True)
         Sxx_z = (Sxx - mu) / sig
-
         
-
         Sxx_epochs.append(Sxx_z)
 
     Sxx_epochs = np.array(Sxx_epochs)
     Sxx_avg = np.mean(Sxx_epochs, axis=0)
 
-
-    
     plt.figure(figsize=(10, 4))
     plt.pcolormesh(t, f, Sxx_avg, shading='gouraud')
     plt.ylabel('Frequency (Hz)')
@@ -73,3 +70,44 @@ def plot_spectrogram(ecog_data, lower_freq, upper_freq, nperseg=256, baseline_co
     plt.tight_layout()
     plt.show()
 
+# applies z-score baseline correction per epoch, then averages the transform
+def plot_morlet(ecog_data, lower_freq, upper_freq, B, C):
+    event_epochs = ecog_data['signal']
+    pre_time, post_time = ecog_data['window']
+    dt = 1 / ecog_data['sampling_rate']
+    freqs = np.arange(lower_freq, upper_freq + 2, 2)
+    scales = pywt.frequency2scale(f'cmor{B}-{C}', freqs * dt)
+
+    wt_epochs = []
+
+    t = np.linspace(-pre_time, post_time, len(event_epochs[0]))
+    b_mask = (t >= -pre_time) & (t <= -pre_time / 2)
+
+    z_sum = 0
+    n_count = 0
+
+    for i in range(event_epochs.shape[0]):
+        signal = event_epochs[i, :]
+        if np.count_nonzero(np.isnan(signal)) > 0:
+            continue
+        cwt, _ = pywt.cwt(signal, scales, f'cmor{B}-{C}', sampling_period=dt)
+        power = np.abs(cwt) ** 2
+
+        mu  = power[:, b_mask].mean(axis=1, keepdims=True)
+        sigma  = power[:, b_mask].std(axis=1,  keepdims=True) + 1e-10
+        z  = (power - mu) / sigma
+
+        z_sum += z
+        n_count += 1
+
+    z_avg = z_sum / n_count
+
+    plt.figure(figsize=(10, 6))
+    plt.imshow(z_avg, extent=[t[0], t[-1], freqs[0], freqs[-1]],
+               cmap='viridis', aspect='auto', origin='lower')
+    plt.colorbar(label='Power')
+    plt.ylabel('Frequency (Hz)')
+    plt.xlabel('Time (s)')
+    plt.title('Morlet Scalogram')
+    plt.tight_layout()
+    plt.show()
