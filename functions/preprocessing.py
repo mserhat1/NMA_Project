38ef73574f532a7056ac453cb36face8543f9f71
Mustafa Serhat Aydın <aydinmustafaserhat@gmail.com>
2025-07-22 21:22:52 +0300

    Update preprocessing.py
    modified spike_cleaning to adjust for new format

--- functions/preprocessing.py
+++ functions/preprocessing.py
@@ -22,38 +22,32 @@ def ptp_thresholding(ecog_data, threshold_uv=200):
 
 # available modes are 'zero' and 'interp'
 def spike_cleaning(ecog_data, z_thresh=5.0, mode='zero', return_spike_mask=False):
-    event_epochs = ecog_data['signal']
-    cleaned_epochs = event_epochs.copy()
+    signal = ecog_data['signal']
+    cleaned_signal = signal.copy()
 
     total_spikes = 0
-    spike_mask_all = np.zeros_like(event_epochs, dtype=bool)
+    spike_mask_all = np.zeros_like(signal, dtype=bool)
 
-    for i in range(event_epochs.shape[0]):
-        epoch = event_epochs[i]
-        z = (epoch - np.mean(epoch)) / (np.std(epoch) + 1e-10)
-        spike_mask = np.abs(z) > z_thresh
-        num_spikes = np.sum(spike_mask)
-        total_spikes += num_spikes
-        spike_mask_all[i] = spike_mask
+    z = (signal - np.mean(signal)) / (np.std(signal) + 1e-10)
+    spike_mask = np.abs(z) > z_thresh
+    num_spikes = np.sum(spike_mask)
 
-        if mode == 'zero':
-            epoch[spike_mask] = 0.0
-        elif mode == 'interp':
-            epoch[spike_mask] = np.interp(np.flatnonzero(spike_mask),
+    if mode == 'zero':
+        signal[spike_mask] = 0.0
+    elif mode == 'interp':
+        signal[spike_mask] = np.interp(np.flatnonzero(spike_mask),
                                           np.flatnonzero(~spike_mask),
-                                          epoch[~spike_mask])
-        else:
-            raise ValueError("mod must be 'zero' or 'interp'")
-
-        cleaned_epochs[i] = epoch
+                                          signal[~spike_mask])
+    else:
+        raise ValueError("mod must be 'zero' or 'interp'")
 
     cleaned_data = ecog_data.copy()
-    cleaned_data['signal'] = cleaned_epochs
+    cleaned_data['signal'] = signal
 
     print(f"Spike cleaning applied: z > {z_thresh} → mode: {mode}")
-    print(f"Total spikes removed/interpolated: {total_spikes}")
+    print(f"Total spikes removed/interpolated: {num_spikes}")
 
     if return_spike_mask:
-        return cleaned_data, spike_mask_all
+        return cleaned_data, spike_mask
     else:
         return cleaned_data
