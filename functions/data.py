ed8fe925cf94d07c3c0a4dd188d8df2b4cf0a7c7
Mustafa Serhat Aydın <aydinmustafaserhat@gmail.com>
2025-07-23 22:50:15 +0300

    Update data.py

--- functions/data.py
+++ functions/data.py
@@ -115,7 +115,7 @@ def extract_spatial_data(nwbfile):
             timestamps = starting_time + np.arange(len(keypoint_data)) / rate
 
         # Store the data and timestamps in the dictionary with the keypoint name as the key
-        spatial_data[keypoint_name] = {'data': keypoint_data, 'timestamps': timestamps, 'sampling_rate': rate}
+        spatial_data[keypoint_name] = {'data': keypoint_data, 'timestamps': timestamps, 'sampling_rate': spatial_series.rate}
     
     return spatial_data
 
